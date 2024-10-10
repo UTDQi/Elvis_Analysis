f5a82351893790e74af4f28d1e5ffb5ee62038df
UTDQi <113342231+UTDQi@users.noreply.github.com>
2024-10-10 19:55:56 -0400

    clean script

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -4,8 +4,7 @@
 # Date: 10 October 2024
 # Contact: david.qi@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: 
-# Any other information needed? 
+# Pre-requisites: None
 
 
 #### Workspace setup ####
@@ -23,10 +22,5 @@ saveRDS(Elvis_Presley , "data/raw/Elvis_Presley.rds")
 Adele <- get_artist_audio_features("Adele")
 saveRDS(Adele , "data/raw/Adele.rds")
 
-
-
-
-#### Save data ####
-#write_csv(the_raw_data, "inputs/data/raw_data.csv") 
 
          
